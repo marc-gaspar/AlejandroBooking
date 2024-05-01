79d26062628b7c0ccaf195a7bf2441c58c246c26
Alejandro <a.infpers.ln@gmail.com>
2024-05-01 15:27:03 +0100

    Project API

--- Booking/Project_Booking/routes/hotels.js
+++ Booking/Project_Booking/routes/hotels.js
@@ -1,69 +1,33 @@
-import express from "express"
-import Hotel from "../models/Hotel.js"
-
+import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req,res)=>{
-    
-    const newHotel = new Hotel(req.body)
+router.post("/", verifyAdmin, createHotel);
 
-    try{
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
 //UPDATE
-router.put("/:id", async (req,res)=>{
-    try{
-        const updateHotel = await Hotel.findByIdAndUpdate(
-            req.params.id, 
-            { $set: req.body },
-            { new: true }
-        );
-        res.status(200).json(savedHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
-
+router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete("/:id", async (req,res)=>{
-    try{
-        await Hotel.findByIdAndUpdate(
-            req.params.id
-        );
-        res.status(200).json("Hotel has been deleted.")
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
-
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET
-router.get("/:id", async (req,res)=>{
-    try{
-        const hotel = await Hotel.findById(
-            req.params.id
-        );
-        res.status(200).json(hotel)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
 
+router.get("/find/:id", getHotel);
 //GET ALL
-router.get("/", async (req,res, next)=>{
 
-    console.log("Hi im a hotel route")
-    next()
-    try{
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
-export default router
+export default router;
